558377d722a02204e9b8a1ec2cb477fe92a87f78
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-23 12:29:07 +0400

    custom animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,18 +11,42 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
 
     var body: some View {
+//        Button("Tap Me") {
+//            // do nothing
+//            tap()
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundStyle(.white)
+//        .clipShape(.circle)
+//        .scaleEffect(animationAmount)
+////        .blur(radius: (animationAmount - 1) * 3)
+////        .animation(.linear, value: animationAmount)
+////        .animation(.spring(duration: 3, bounce: 0.9), value: animationAmount)
+//        .animation(.easeInOut(duration: 1).repeatCount(5, autoreverses: false), value: animationAmount)
+//
+
         Button("Tap Me") {
-            // do nothing
-            tap()
+            // animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
-
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 3
+        }
     }
     
     func tap() {
