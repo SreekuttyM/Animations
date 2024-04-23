32f1829826f1a0f522996b9f3c7d4b0839c918ea
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-23 12:35:04 +0400

    adding animation changes binded to the stepper

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,41 +11,21 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
 
     var body: some View {
-//        Button("Tap Me") {
-//            // do nothing
-//            tap()
-//        }
-//        .padding(50)
-//        .background(.red)
-//        .foregroundStyle(.white)
-//        .clipShape(.circle)
-//        .scaleEffect(animationAmount)
-////        .blur(radius: (animationAmount - 1) * 3)
-////        .animation(.linear, value: animationAmount)
-////        .animation(.spring(duration: 3, bounce: 0.9), value: animationAmount)
-//        .animation(.easeInOut(duration: 1).repeatCount(5, autoreverses: false), value: animationAmount)
-//
+        print(animationAmount)
 
-        Button("Tap Me") {
-            // animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 3
+       return VStack {
+           Stepper("Scale amount", value: $animationAmount.animation(.easeInOut(duration: 1).repeatCount(2)), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
     }
     
