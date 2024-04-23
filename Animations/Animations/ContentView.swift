9a29e15c6680abf37cc2087851d0f2d07fee8161
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-23 12:41:19 +0400

    3drotationeffect animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,22 +11,18 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
 
     var body: some View {
-        print(animationAmount)
 
-       return VStack {
-           Stepper("Scale amount", value: $animationAmount.animation(.easeInOut(duration: 1).repeatCount(2)), in: 1...10)
-            
-            Spacer()
-            
             Button("Tap Me") {
-                animationAmount += 1
+                withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                    animationAmount += 360
+                }
             }
             .padding(40)
             .background(.red)
             .foregroundStyle(.white)
             .clipShape(.circle)
             .scaleEffect(animationAmount)
-        }
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
     
     func tap() {
